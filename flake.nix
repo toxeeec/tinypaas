7ff3b046bd196cb779d2106131a36c08c55c16a7
toxeeec <bartosz.kapciak@gmail.com>
2026-07-09 00:57:06 +0200

    chore: add pre-commit hooks

--- flake.nix
+++ flake.nix
@@ -1,19 +1,41 @@
 {
   inputs = {
+    git-hooks.url = "github:cachix/git-hooks.nix";
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
   };
-  outputs = {nixpkgs, ...}: let
+  outputs = {
+    self,
+    git-hooks,
+    nixpkgs,
+    ...
+  }: let
     forAllSystems = function:
       nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed (
-        system: function nixpkgs.legacyPackages.${system}
+        system: function system nixpkgs.legacyPackages.${system}
       );
   in {
-    formatter = forAllSystems (pkgs: pkgs.alejandra);
+    formatter = forAllSystems (_system: pkgs: pkgs.alejandra);
 
-    devShells = forAllSystems (pkgs: {
-      default = pkgs.mkShell {
-        packages = [];
+    checks = forAllSystems (system: pkgs: {
+      pre-commit-check = git-hooks.lib.${system}.run {
+        src = ./.;
+        package = pkgs.prek;
+
+        hooks = {
+          alejandra.enable = true;
+          statix.enable = true;
+        };
       };
+    });
+
+    devShells = forAllSystems (system: pkgs: {
+      default = let
+        inherit (self.checks.${system}.pre-commit-check) shellHook enabledPackages;
+      in
+        pkgs.mkShell {
+          inherit shellHook;
+          packages = enabledPackages;
+        };
     });
   };
 }
