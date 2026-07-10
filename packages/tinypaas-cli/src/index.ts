0f1345727b97ae1d72c09b5e587f7c7a4239e5e2
toxeeec <bartosz.kapciak@gmail.com>
2026-07-10 02:17:03 +0200

    feat(cli): add deploy subcommand

--- packages/tinypaas-cli/src/index.ts
+++ packages/tinypaas-cli/src/index.ts
@@ -1,9 +1,14 @@
 import { NodeServices, NodeRuntime } from "@effect/platform-node"
 import { Console, Effect } from "effect"
-import { Command } from "effect/unstable/cli"
+import { Argument, Command } from "effect/unstable/cli"
 
 import packageJson from "../package.json" with { type: "json" }
 
-const hello = Command.make("hello", {}, () => Console.log("Hello, world!"))
-const program = Command.run(hello, { version: packageJson.version })
+const path = Argument.path("path", { pathType: "directory", mustExist: true }).pipe(
+	Argument.withDefault(process.cwd()),
+)
+const deploy = Command.make("deploy", { path }, ({ path }) => Console.log(path))
+
+const tps = Command.make("tps").pipe(Command.withSubcommands([deploy]))
+const program = Command.run(tps, { version: packageJson.version })
 program.pipe(Effect.provide(NodeServices.layer), NodeRuntime.runMain)
